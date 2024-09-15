8b025b256d2aa6dca0c67965545f6dd0bd3729bd
Mustakim4 <mustakquimdiwan@gmail.com>
2024-09-15 09:42:11 +0530

    first commit

---
+++ app/config/connection.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+require('dotenv').config();
+let connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASS,
+        database: 'todos_db'
+    });
+}
+
+connection.on('connect', () => {
+    console.log(`--> Connected to database '${connection.config.database}'.`);
+});
+
+connection.on('error', err => {
+    console.log('--> Connection error: ', err);
+});
+
+connection.connect();
+
+module.exports = connection;
